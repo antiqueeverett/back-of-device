cf52caeb4d9a4868296db72eeade5d0472e45fe2
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2018-08-20 16:20:11 +0200

    WIP: centered media player interface

--- development/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ development/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Everett\\Dropbox\\gitHub\\cs\\android\\mis-2018-project-Back-of-Device\\development\\openCVLibrary341\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Everett\\Dropbox\\gitHub\\cs\\android\\back-of-device\\development\\openCVLibrary341\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
